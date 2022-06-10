74df9a74779e7b065eadd958d505ce15cea923ad
Nguyễn Ngọc Hưng <Hungxhq@outlook.com>
2022-06-11 00:45:57 +0700

    commit

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ tabs.forEach(tab =>{
     tab.addEventListener('click',()=>{
         const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tc => {
+        tabContents.forEach(tc => { 
             tc.classList.remove('filters__active')
         })
         target.classList.add('filters__active')
